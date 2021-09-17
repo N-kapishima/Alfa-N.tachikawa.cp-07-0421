e34564e24e019921f967812bdf7813fc0963631f
ミシマハルト <89884469+N-kapishima@users.noreply.github.com>
2021-09-17 09:56:08 +0900

    Update main.js

--- assets/js/main.js
+++ assets/js/main.js
@@ -18,7 +18,7 @@ const app = Vue.createApp({
       */
       correctAnswer: {
         stage1: {
-          q1: '238107',
+          q1: '234107',
         },
         stage2: {
           q1: 'いいい',
